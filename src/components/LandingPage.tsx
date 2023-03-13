89147aa88bb9ea89ccc0c868c97be17028454e61
alxndrdrgz <alexander.e.rsa@gmail.com>
2023-03-13 15:27:41 -0400

    Adds barebones landing page component

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,5 +1,7 @@
 const LandingPage = ({ user }: { user: any }) => {
-  return <>{user && <h1>Welcome! {user}</h1>}</>;
+  return <>
+          {user && <h1>Welcome! {user}</h1>}
+         </>;
 };
 
 export default LandingPage;
