3ae66527d5dfc0698fef31c55c1f7dcb9c5d9402
alxndrdrgz <alexander.e.rsa@gmail.com>
2023-03-14 16:59:53 -0400

    First working version of WeatherForecast component

--- src/components/LandingPage.tsx
+++
@@ -1,7 +0,0 @@
-const LandingPage = ({ user }: { user: any }) => {
-  return <>
-          {user && <h1>Welcome! {user}</h1>}
-         </>;
-};
-
-export default LandingPage;
--- src/components/WeatherForecast.jsx
+++ src/components/WeatherForecast.jsx
@@ -1,11 +1,18 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-
-const WeatherForecast = () => {
+const WeatherForecast = ({ data }) => {
   
   return (
     <div>
-
+      <h1 className="font-bold">City: {data.name}</h1>
+      {data.weather.map((i) => (
+        <div key={i}>
+          <p>{i.main}</p>
+          <p>{i.description}</p>
+        </div>
+      ))}
+      <div>
+        <p>Temperature: {data.main.temp}</p>
+        <p>Feels like: {data.main.feels_like}</p>
+      </div>
     </div>
   )
 };
