9761224a29a8abcff53ea83d89a38856975b8157
alxndrdrgz <alexander.e.rsa@gmail.com>
2023-03-14 13:45:33 -0400

    Weather component scaffolding WIP

---
+++ src/components/WeatherForecast.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const WeatherForecast = () => {
+  
+  return (
+    <div>
+
+    </div>
+  )
+};
+
+
+export default WeatherForecast;
